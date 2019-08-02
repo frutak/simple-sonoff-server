ffd45eaee1d179cb3f19bb96604fe9cdb73cbcec
frutak <42648972+frutak@users.noreply.github.com>
2019-08-02 13:02:46 +0200

    Update simple.js

--- simple.js
+++ simple.js
@@ -9,30 +9,22 @@ app.use(express.static(__dirname + "/"))
 var server = http.createServer(app)
 server.listen(port)
 
+
+
 console.log("http server listening on %d", port)
 
 var wss = new WebSocketServer({server: server})
-
 console.log("websocket server created")
 
 wss.on("connection", function(ws) {
-  ws.on("connection", function (str) {
-    console.log("websocket connection open")
-    var data = JSON.parse(str)
-    console.log("data %s", JSON.stringify(data))
-    res = {
-                "error": 0,
-                "deviceid": data.deviceid,
-                "apikey": "111111111-1111-1111-1111-111111111111"
-            }
-    var r = JSON.stringify(res);
-    ws.sendText(r);
-  })
+  var id = setInterval(function() {
+    ws.send(JSON.stringify(new Date()), function() {  })
+  }, 1000)
 
+  console.log("websocket connection open")
 
   ws.on("close", function() {
     console.log("websocket connection close")
     clearInterval(id)
   })
 })
-
