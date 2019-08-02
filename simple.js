c2e168e37d1df5ef4621cf51d88b0bdd565da726
frutak <42648972+frutak@users.noreply.github.com>
2019-08-02 11:27:05 +0200

    Create simple.js

---
+++ simple.js
@@ -0,0 +1,34 @@
+var WebSocketServer = require("ws").Server
+var http = require("http")
+var express = require("express")
+var app = express()
+var port = process.env.PORT || 5000
+
+app.use(express.static(__dirname + "/"))
+
+var server = http.createServer(app)
+server.listen(port)
+
+console.log("http server listening on %d", port)
+
+var wss = new WebSocketServer({server: server})
+console.log("websocket server created")
+
+wss.on("connection", function (str) {
+    var data = JSON.parse(str);
+    console.log("data %s", JSON.stringify(data));
+    res = {
+                "error": 0,
+                "deviceid": data.deviceid,
+                "apikey": "111111111-1111-1111-1111-111111111111"
+            };
+        
+  }, 1000)
+
+  console.log("websocket connection open")
+
+  ws.on("close", function() {
+    console.log("websocket connection close")
+    clearInterval(id)
+  })
+})
